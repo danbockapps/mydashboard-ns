dd73ed43dc4b852aaa1541bde1659619cbab0bbe
Dan Bock <danbock@gmail.com>
2018-05-17 17:14:27 -0400

    Add weight graph component

--- app/tabs/tabs.module.ts
+++ app/tabs/tabs.module.ts
@@ -6,17 +6,23 @@ import { HomeComponent } from "./home/home.component";
 import { SearchComponent } from "./search/search.component";
 import { TabsRoutingModule } from "./tabs-routing.module";
 import { TabsComponent } from "./tabs.component";
+import { WeightGraphComponent } from "~/weight-graph/weight-graph.component";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+import { NativeScriptUIChartModule } from "nativescript-ui-chart/angular";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
+        NativeScriptHttpModule,
+        NativeScriptUIChartModule,
         TabsRoutingModule
     ],
     declarations: [
         TabsComponent,
         HomeComponent,
         BrowseComponent,
-        SearchComponent
+        SearchComponent,
+        WeightGraphComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
