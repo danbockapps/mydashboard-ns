79811388e213752b8cbc9da36133eea676e3a316
Dan Bock <danbock@gmail.com>
2018-05-22 08:31:51 -0400

    Add logo to login screen

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -9,7 +9,8 @@ import { Config } from "~/shared/config";
   selector: "Login",
   providers: [UserService],
   moduleId: module.id,
-  templateUrl: "./login.component.html"
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
   user: User;
